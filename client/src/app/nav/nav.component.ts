143f9eccc72040052f9712290cb5f1b3b1b3b627
nafizrifat <imtiaz_rifat@yahoo.com>
2025-04-08 21:52:55 -0500

    Routing in Angular

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,30 +3,34 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { NgIf } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule,NgIf,BsDropdownModule],
+  imports: [FormsModule,NgIf,BsDropdownModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   model: any={};
   accountService=inject(AccountService);
-
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: response=>{
-        console.log(response);
+      next: ()=>{
+        this.router.navigateByUrl('/members');
       },
-      error:error=>console.log(error)      
+      error:error=>this.toastr.error(error.error)
     })
   }
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, inject, Input, input, output, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-register',
@@ -11,6 +13,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
+    private toastr = inject(ToastrService);
 
   // @Input() usersFromHomeComponent:any;
   // @Output() cancelRegister = new EventEmitter();
@@ -27,7 +30,7 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error=>console.log(error)
+      error: error=>this.toastr.error(error.error)
     });
   }
 
